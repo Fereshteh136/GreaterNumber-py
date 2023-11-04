1cd3db7686fcc44814bc4ba8f1af378d34d10b6e
Fereshteh136 <149617279+Fereshteh136@users.noreply.github.com>
2023-11-04 13:36:42 +0330

    Add files via upload

---
+++ GreaterNumber.py
@@ -0,0 +1,12 @@
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Compare the numbers and print the result
+if num1 > num2:
+    print(f"{num1} is greater than {num2}.")
+elif num1 < num2:
+    print(f"{num2} is greater than {num1}.")
+else:
+    print("Both numbers are equal.")
+
